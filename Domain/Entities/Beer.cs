511f09c503b012469ddc0d78b7f661016eafcc18
kristian meyer <183391596+kristianmeyer1@users.noreply.github.com>
2025-12-15 16:01:42 +0100

    Added beer class

--- Domain/Entities/Beer.cs
+++ Domain/Entities/Beer.cs
@@ -3,5 +3,14 @@ namespace SkumOgSandhed.Domain
     public class Beer
     {
         public int BeerId { get; set; }
+        public int ReleaseYear { get; set; }
+        public int DrinkingYear { get; set; }
+        public decimal Abv { get; set; }
+        public string FoodPairing { get; set; }
+        public string Description { get; set; }
+        public string Fermentation { get; set; }
+        public decimal Price { get; set; }
+        public int Rating { get; set; }
+        public int UntappdRating { get; set; }
     }
 }
